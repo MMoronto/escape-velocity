e066f198d669d8669d7136b4ac74c48ab43b2012
MMORONTO <t2wycemo@gmail.com>
2021-04-01 16:33:06 -0400

    setup slugify and avg_score methods in orbiter model

--- app/models/orbiter.rb
+++ app/models/orbiter.rb
@@ -1,2 +1,15 @@
 class Orbiter < ApplicationRecord
+    has_many :reviews
+
+    before_create :slugify
+
+    def slugify
+        self.slug = name.parametrize
+    end
+
+    def avg_score
+        return 0 unless reviews.size.positive?
+
+        reviews.average(:score).to_f.round(2)
+    end
 end
