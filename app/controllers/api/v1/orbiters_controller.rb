8cc686702ac292cec31cc87fffb95081d5cf49af
MMORONTO <t2wycemo@gmail.com>
2021-04-02 14:39:39 -0400

    add CRUD actions for orbiter controller class

--- app/controllers/api/v1/orbiters_controller.rb
+++ app/controllers/api/v1/orbiters_controller.rb
@@ -6,6 +6,48 @@ module Api
 
                 render json: OrbiterSerializer.new(orbiters).serialized_json
             end
+
+            def show
+                orbiter = Orbiter.find_by(slug: params[:slug])
+
+                render json: OrbiterSerializer.new(orbiter).serialized_json
+            end
+
+            def create
+                orbiter = Orbiter.new(orbiter_params)
+
+                if orbiter.save
+                    render json: OrbiterSerializer.new(orbiter).serialized_json
+                else
+                    render json: {error: orbiter.errors.messages}, status: 422
+                end
+            end
+
+            def update
+                orbiter = Orbiter.find_by(slug: params[:slug])
+
+                if orbiter.update(orbiter_params)
+                    render json: OrbiterSerializer.new(orbiter).serialized_json
+                else
+                    render json: {error: orbiter.errors.messages}, status: 422
+                end
+            end    
+
+            def destroy
+                orbiter = Orbiter.find_by(slug: params[:slug])
+
+                if orbiter.destroy
+                    head :no_content
+                else
+                    render json: {error: orbiter.errors.messages}, status: 422
+                end
+            end            
+
+            private
+
+            def orbiter-params
+                params.require(:orbiter).permit(:name, :image_url)
+            end
         end
     end
 end
