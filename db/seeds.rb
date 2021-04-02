bdf87ca1973456aa4b3de63053532d160ecc91c7
MMORONTO <t2wycemo@gmail.com>
2021-04-02 09:10:22 -0400

    seed database with some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Orbiter.create([
+    {
+        name: "Blue Origin",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/blue-originLogo.png"
+    },
+    {
+        name: "NASA Commercial",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/NASA-logo.png"
+    },
+    {
+        name: "Roscosmos",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/roscosmos-logo.png"
+    },
+    {
+        name: "Space Adventures",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/Space-Adventures-logo.png"
+    },
+    {
+        name: "Space Florida",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/space-florida-logo.png"
+    },
+    {
+        name: "Space X Commercial",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/space-x-logo.png"
+    },
+    {
+        name: "Spaceport America",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/spaceport_america-logo.png"
+    },
+    {
+        name: "Virgin Galactic",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/Virgin-Galactic-logo.png"
+    },
+    {
+        name: "Virgin Orbit",
+        image_url: "https://e-velocity.s3.us-east-2.amazonaws.com/Virgin-Orbit-logo.png"
+    }                  
+])
