c1ce0bac3d882a8598cf256452adab804d6a5510
MMORONTO <t2wycemo@gmail.com>
2021-04-02 16:51:18 -0400

    test CRUD functionality/debug orbiters & reviews controller classes via postman

--- app/controllers/api/v1/orbiters_controller.rb
+++ app/controllers/api/v1/orbiters_controller.rb
@@ -1,6 +1,7 @@
 module Api
     module V1
         class OrbitersController < ApplicationController
+            protect_from_forgery with: :null_session
             def index
                 orbiters = Orbiter.all
 
