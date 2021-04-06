531f3c2a606a91eca4659262c2edf04f348bc29f
MMORONTO <t2wycemo@gmail.com>
2021-04-06 13:46:24 -0400

    Replace placeholder text in render method with layout/content

--- app/javascript/components/Orbiters/Orbiters.js
+++ app/javascript/components/Orbiters/Orbiters.js
@@ -4,7 +4,15 @@ const Orbiters = () => {
     const [orbiters, setOrbiters] = useState([])
 
     return(
-        <div>This is the Orbiters#index page.</div>
+        <div className="home">
+            <div className="header">
+                <h1>EscapeVelocity</h1>
+                <p className="subheader">Honest, unbiased launch vehicle reviews. Share your experience.</p>                
+            </div>
+            <div className="grid">
+                ORBITERS GRID GOES HERE
+            </div>
+        </div>
     )
 }
 
