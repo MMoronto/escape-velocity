c5a587cdccebcb5aa05e49ef2d6029d62678c672
MMORONTO <t2wycemo@gmail.com>
2021-04-08 15:54:55 -0400

    debug serializer issue preventing display of data

--- app/javascript/components/Orbiters/Orbiters.js
+++ app/javascript/components/Orbiters/Orbiters.js
@@ -1,11 +1,11 @@
-import React, { useState, useEffect, Fragment } from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import Orbiter from './Orbiter'
 
 const Orbiters = () => {
     const [orbiters, setOrbiters] = useState([])
 
-    useEffect(() => {
+    useEffect( () => {
         axios.get('api/v1/orbiters.json')
         .then( resp => {
             setOrbiters(resp.data.data) 
@@ -14,10 +14,15 @@ const Orbiters = () => {
     }, [orbiters.length])
 
     const grid = orbiters.map( item => {
+        // const { name, image_url, slug, average_score } = orbiter.attributes
+
         return (
             <Orbiter 
                 key={item.attributes.name}
                 attributes={item.attributes}
+                // image_url={image_url}
+                // slug={slug}
+                // average_score={average_score}
             />
         )
     })
