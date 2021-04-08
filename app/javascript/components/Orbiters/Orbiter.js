8062484edb63c38584ba5bf03fded1652da4f8ab
MMORONTO <t2wycemo@gmail.com>
2021-04-08 10:45:19 -0400

    Setup react component to receive  data from api

--- app/javascript/components/Orbiters/Orbiter.js
+++ app/javascript/components/Orbiters/Orbiter.js
@@ -1,7 +1,15 @@
 import React from 'react'
 
 const Orbiter = (props) => {
-    return ()
+    const {name, image_url, slug} = props.attributes
+    return (
+        <div className="card">
+
+            <div className="orbital-logo">
+                <img src={image_url} alt={name} width="50" />
+            </div>
+        </div>
+    )
 }
 
 export default Orbiter
