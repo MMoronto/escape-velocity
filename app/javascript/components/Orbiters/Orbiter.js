0cb3643ab8d269fbb15f87f86047994b2654cab4
MMORONTO <t2wycemo@gmail.com>
2021-04-08 10:49:32 -0400

    set up react component

--- app/javascript/components/Orbiters/Orbiter.js
+++ app/javascript/components/Orbiters/Orbiter.js
@@ -2,12 +2,22 @@ import React from 'react'
 
 const Orbiter = (props) => {
     const {name, image_url, slug} = props.attributes
+    
     return (
         <div className="card">
 
             <div className="orbital-logo">
                 <img src={image_url} alt={name} width="50" />
             </div>
+
+            <div className="orbiter-name">
+                {name}
+            </div>
+
+            <div className="link-wrapper">
+                <a href={"/" + slug}>View Orbiter</a>
+            </div>
+
         </div>
     )
 }
