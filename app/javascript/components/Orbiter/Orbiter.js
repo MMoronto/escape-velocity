ef25e0284928e45b58073566a8dab6824e00ea44
MMORONTO <t2wycemo@gmail.com>
2021-04-09 15:00:01 -0400

    Build Orbiters#show view. Set orbiter & review to empty objects

--- app/javascript/components/Orbiter/Orbiter.js
+++ app/javascript/components/Orbiter/Orbiter.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
 const Orbiter = () => {
+    const [orbiter, setOrbiter] = useState({})
+    const [review, setReview] = useState({})
+    
     return(
         <div>This is the Orbiters#show page for our app.</div>
     )
