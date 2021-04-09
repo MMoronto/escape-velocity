d6110162ac9546db36b7792f151cfab55509662c
MMORONTO <t2wycemo@gmail.com>
2021-04-09 15:04:02 -0400

    add useEffect hook and method to react component

--- app/javascript/components/Orbiter/Orbiter.js
+++ app/javascript/components/Orbiter/Orbiter.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const Orbiter = () => {
     const [orbiter, setOrbiter] = useState({})
     const [review, setReview] = useState({})
-    
+
+    useEffect(() =>{
+        // add code here
+    }, [])
+
     return(
         <div>This is the Orbiters#show page for our app.</div>
     )
