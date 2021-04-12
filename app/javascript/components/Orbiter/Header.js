78da24ee21ca83a82f104bf509f59b672fb3db94
MMORONTO <t2wycemo@gmail.com>
2021-04-12 08:43:29 -0400

    build Orbiters#show page layout

---
+++ app/javascript/components/Orbiter/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Header = (props) => {
+    const {name, image_url, avg_score} = props.attributes
+    const total = props.review.length
+
+    return (
+        <div className="Wrapper">
+            <h1> <img src="img_url" alt="name" /> {name}</h1>
+            <div>
+                <div className="totalReviews">{total} User Reviews</div>
+                <div className="starRating"></div>
+                <div className="totalOutOf">{avg_score} out of 5</div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
--- app/javascript/components/Orbiter/Orbiter.js
+++ app/javascript/components/Orbiter/Orbiter.js
@@ -1,20 +1,40 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import Header from './Header'
 
 const Orbiter = (props) => {
     const [orbiter, setOrbiter] = useState({})
     const [review, setReview] = useState({})
+    const [loaded, setLoaded] = useState(false)
 
     useEffect(()=>{
         const slug = props.match.params.slug
         const url = `/api/v1/orbiters/${slug}`
 
         axios.get(url)
-        .then( resp => console.log(resp) )
+        .then( resp => {
+            setOrbiter(resp.data)
+            setLoaded(true)
+        })
         .catch( resp => console.log(resp) )
     }, [])
 
-    return <div>This is the Orbiters#show page for our app.</div>
+    return (
+        <div className="wrapper">
+            <div className="column">
+                { 
+                    loaded &&
+                <Header 
+                    attributes={orbiter.data.attributes}
+                />
+                }
+                <div className="reviews"></div>
+            </div>
+            <div className="column">
+                <div className="review-form">[Review Form Goes Here.]</div>
+            </div>
+        </div>
+    )
     
 }
 
