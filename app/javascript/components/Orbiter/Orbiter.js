fc808bd0505cb31ab084e63a2949d9a090bb8dc2
MMORONTO <t2wycemo@gmail.com>
2021-04-12 08:00:27 -0400

    log API request to console. Verify response from API

--- app/javascript/components/Orbiter/Orbiter.js
+++ app/javascript/components/Orbiter/Orbiter.js
@@ -1,27 +1,21 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 
-const Orbiter = () => {
+const Orbiter = (props) => {
     const [orbiter, setOrbiter] = useState({})
     const [review, setReview] = useState({})
 
-    useEffect(() =>{
-        const slug = this.props.match.params.slug
-        // const url = (`api/v1/${slug}`)
-        const url = 'api/v1/orbiters/' + slug
+    useEffect(()=>{
+        const slug = props.match.params.slug
+        const url = `/api/v1/orbiters/${slug}`
 
         axios.get(url)
-        .then( (resp) => {
-            debugger
-        })
-        .catch( data => {
-            debugger
-        })
+        .then( resp => console.log(resp) )
+        .catch( resp => console.log(resp) )
     }, [])
 
-    return(
-        <div>This is the Orbiters#show page for our app.</div>
-    )
+    return <div>This is the Orbiters#show page for our app.</div>
+    
 }
 
 export default Orbiter
