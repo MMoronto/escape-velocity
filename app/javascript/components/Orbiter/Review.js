3387e6c3c64ec222287249857c7a202a8ddc9378
MMORONTO <t2wycemo@gmail.com>
2021-04-16 08:42:52 -0400

    style review component. format elements of review component

--- app/javascript/components/Orbiter/Review.js
+++ app/javascript/components/Orbiter/Review.js
@@ -1,16 +1,34 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Card = styled.div`
+    border: 1px solid rgba(0,0,0,0.1);
+    border-radius: 4px;
+    padding: 20px;
+    margin: 0 20px 20px 0;
+`
+const RatingContainer = styled.div``
+const RatingScore = styled.div``
+const Title = styled.div`
+    padding: 20px 0 0 0;
+    font-size: 18px;
+`
+const Description = styled.div`
+    padding: 0 0 20px 0;
+    font-size: 14px;
+`
 
 const Review = (props) => {
     const { score, title, description } = props.attributes
 
     return (
-        <div className="card">
+        <Card>
             <div className="rating-container">
                 <div className="rating-score">{score}</div>
             </div>
-            <div className="title">{title}</div>
-            <div className="description">{description}</div>
-        </div>
+            <Title>{title}</Title>
+            <Description>{description}</Description>
+        </Card>
     )
 }
 
